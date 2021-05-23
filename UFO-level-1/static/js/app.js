6a7b6a2b8a914a8793a8ea4ac74a443d0e16053b
teals <tealiie@live.com>
2021-05-23 16:11:23 +1200

    Table populates filtered date input

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,20 +1,43 @@
-// from data.js
-// var tableData = data;
-
-
-
-// YOUR CODE HERE!
+// the data from data.js
+var tableData = data;
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
 // Console.log the weather data from data.js
-console.log(data);
+console.log(tableData);
 
-data.forEach((ufoReport) => {
+// populate the data into a table
+tableData.forEach((ufoReport) => {
     var row = tbody.append("tr");
     Object.entries(ufoReport).forEach(([key, value]) => {
       var cell = row.append("td");
       cell.text(value);
     });
   });
+
+// select the button
+var button = d3.select("#filter-btn");
+
+// create event handlers
+button.on("click", runEnter);
+
+function runEnter() {
+    var inputDate = d3.select("#datetime").property("value");
+    console.log(inputDate);
+
+    filteredtable = tableData.filter(row =>  row.datetime === inputDate);
+    console.log(filteredtable);
+
+    // clear table 
+    tbody.html("");
+
+    // return filtered data
+    filteredtable.forEach((ufoReport) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoReport).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+  };
